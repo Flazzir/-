89b194a39aa28222bc0de6ea7a4da1021271439f
Flazzir <99480064+Flazzir@users.noreply.github.com>
2024-10-16 22:24:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Плавне прокручування
     const smoothScrollLinks = document.querySelectorAll('.smooth-scroll');
-    
     smoothScrollLinks.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
             const targetId = this.getAttribute('href').substring(1);
             const targetElement = document.getElementById(targetId);
-            
             window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: 'smooth'
-            });
-        });
-    });
-});
+                top:
